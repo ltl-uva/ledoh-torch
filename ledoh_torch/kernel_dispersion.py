a0bbc4592b6cb5b9290d79b382a21a94c3004dee
Evgeniia <vremendm@gmail.com>
2023-12-26 17:39:34 +0100

    add batch mode

--- ledoh_torch/kernel_dispersion.py
+++ ledoh_torch/kernel_dispersion.py
@@ -1,7 +1,12 @@
 import torch
 from torch import Tensor
 
-def cosine_kernel_dispersion(X: Tensor, gamma: float):
-    similarities = X @ X.T
+def cosine_kernel_dispersion(X: Tensor, gamma: float, sample_size=-1):
+    if sample_size<0:
+        sample_size=X.shape[0]
+    batch_idx=torch.randperm(X.shape[0], device=X.device)[:sample_size]
+    X_batch = torch.index_select(X, 0, batch_idx)
+    print(X_batch.shape)
+    similarities = X_batch @ X_batch.T
     similarities.fill_diagonal_(0)
     return torch.exp(gamma*similarities).sum()
