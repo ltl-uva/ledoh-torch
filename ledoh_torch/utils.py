c2f3ca88f5a2707450c30f8e295dbe42d9c69b9a
Evgeniia <vremendm@gmail.com>
2024-02-13 10:55:33 +0100

    annotation

--- ledoh_torch/utils.py
+++ ledoh_torch/utils.py
@@ -1,18 +1,16 @@
+from typing import Tuple
 import torch
 
 
-@torch.jit.script
 def minimum_acos_distance(X: torch.Tensor) -> torch.Tensor:
     return torch.acos((X @ X.T).clamp(-1 + 1e-4, 1 - 1e-4)).fill_diagonal_(float('inf')).view(-1).min()
 
 
-@torch.jit.script
 def circular_variance(X: torch.Tensor) -> torch.Tensor:
     return 1 - torch.norm(torch.mean(X, dim=0))
 
 
-@torch.jit.script
-def init_great_circle(d, dtype=None, device=None):
+def init_great_circle(d: int, dtype=None, device: str = None) -> Tuple[torch.Tensor, torch.Tensor]:
     PQ = torch.randn(d, 2,
                      dtype=dtype,
                      device=device)
