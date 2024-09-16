a1c65ceb203d2254e5c9b4659250a0dfbe283187
Evgeniia <vremendm@gmail.com>
2024-09-16 16:23:36 +0200

    reorganize and put pairwise dispersions into the same place

--- ledoh_torch/__init__.py
+++ ledoh_torch/__init__.py
@@ -1,36 +1,24 @@
-from ledoh_torch.lloyd_dispersion import LloydSphereDispersion
-from ledoh_torch.kernel_dispersion import KernelSphereDispersion
-from ledoh_torch.kernel_semibatched_dispersion import KernelSphereSemibatchDispersion
-from ledoh_torch.sliced_dispersion import SlicedSphereDispersion
-from ledoh_torch.sliced_dispersion import AxisAlignedSlicedSphereDispersion
-from ledoh_torch.sliced_batch import AxisAlignedBatchSphereDispersion
-from ledoh_torch.mma_dispersion import MMADispersion
-from ledoh_torch.sphere_dispersion import SphereDispersion
-
-from ledoh_torch.utils import init_great_circle, \
-    minimum_acos_distance, \
-    circular_variance, \
-    minimum_acos_distance_batch, \
-    minimum_acos_distance_row, \
-    avg_acos_distance, \
-    avg_acos_distance_batch, median_acos_distance, median_acos_distance_batch
+from lloyd_dispersion import *
+from pairwise_dispersion import *
+from sliced_dispersion import *
+from sliced_batch import *
+from utils import *
 
 __all__ = [
-    "LloydSphereDispersion",
     "KernelSphereDispersion",
-    "KernelSphereSemibatchDispersion",
+    "LloydSphereDispersion",
     "SlicedSphereDispersion",
-    "AxisAlignedSlicedSphereDispersion",
-    "AxisAlignedBatchSphereDispersion",
-    "SphereDispersion",
     "MMADispersion",
+    "AxisAlignedBatchSphereDispersion",
+    "MMCSDispersion",
+    "MHEDispersion",
+    "KoLeoDispersion",
     "init_great_circle",
-    "minimum_acos_distance",
-    "avg_acos_distance",
-    "minimum_acos_distance_batch",
-    "circular_variance",
     "minimum_acos_distance_row",
-    "avg_acos_distance_batch",
-    "median_acos_distance",
+    "minimum_acos_distance_batch",
     "median_acos_distance_batch",
+    "median_acos_distance",
+    "avg_acos_distance",
+    "avg_acos_distance_batch",
+    "circular_variance",
 ]
---
+++ ledoh_torch/pairwise_dispersion.py
@@ -0,0 +1,83 @@
+import torch
+from torch import Tensor
+import torch.nn.functional as F
+from .sphere_dispersion import SphereDispersion
+
+@torch.jit.script
+def get_batched_dot_product(X: Tensor, batch_size: int):
+    batch_size = X.shape[0] if batch_size < 0 else batch_size
+
+    batch_idx = torch.randperm(X.shape[0], device=X.device)[:batch_size]
+    X_batch = torch.index_select(X, 0, batch_idx)
+
+    similarities = X_batch @ X_batch.T
+    return similarities
+
+class PairwiseDispersion(SphereDispersion):
+    def __init__(self, batch_size: int = -1, eps:float = 1e-7):
+        super().__init__()
+        self.batch_size = batch_size
+        self.eps = eps
+
+class KernelSphereDispersion(PairwiseDispersion):
+    def __init__(self, gamma: float = 1.0, batch_size: int = -1):
+        """Dispersion of a set of points on the sphere using kernel function.
+           :param gamma: scaling factor
+           :param batch_size: reduce the memory usage by computing the dispersion on a batch of size batch_size
+           :return: loss value, {"sample_size": batch_size}
+        """
+        super().__init__(batch_size)
+        self.gamma = gamma
+
+    def forward(self, X:Tensor) -> Tensor:
+        similarities = get_batched_dot_product(X, self.batch_size)
+        similarities = similarities-torch.triu(similarities)
+        cnt = 2.0 / (self.batch_size * (self.batch_size - 1))
+        loss = torch.exp(self.gamma * similarities).sum() * cnt
+        return loss
+
+class MMADispersion(PairwiseDispersion):
+    def __init__(self, batch_size: int = -1, eps=1e-7):
+        super().__init__(batch_size=batch_size, eps=eps)
+
+    def forward(self, X: Tensor) -> Tensor:
+        similarities = get_batched_dot_product(X, self.batch_size)
+        angles = torch.arccos(similarities.clamp(-1 + self.eps, 1 - self.eps))
+        angles = angles.fill_diagonal_(torch.inf)
+        return -angles.min(dim=1)[0].mean()
+
+class MMCSDispersion(PairwiseDispersion):
+    def forward(self, X: Tensor) -> Tensor:
+        similarities = get_batched_dot_product(X, self.batch_size) + 1
+        similarities -= 2. * torch.diag(torch.diag(similarities))
+        loss = similarities.max(dim=1)[0]
+        return loss.mean()
+
+class MHEDispersion(PairwiseDispersion):
+    def __init__(self, batch_size: int = -1, eps=1e-7, s_power=0):
+        super().__init__(batch_size=batch_size, eps=eps)
+        self.s_power = s_power
+
+    def forward(self, X: Tensor) -> Tensor:
+        similarities = get_batched_dot_product(X, self.batch_size)
+        angles = torch.arccos(similarities.clamp(-1 + self.eps, 1 - self.eps))
+        angles = angles-torch.triu(angles)
+        cnt = 2.0 / (self.batch_size * (self.batch_size - 1))
+        loss = 0
+        if self.s_power == 0:
+            loss = -torch.log(angles).sum() * cnt
+
+        if self.s_power>0:
+            loss = (torch.pow(angles, torch.ones_like(angles)*-self.s_power)).sum() * cnt
+
+        return loss
+
+
+class KoLeoDispersion(PairwiseDispersion):
+    def forward(self, X: Tensor) -> Tensor:
+        pdist = F.pairwise_distance(X, X, p=2.0, eps=self.eps, keepdim=True)
+        pdist = pdist.fill_diagonal_(torch.inf)
+        loss = torch.log(pdist.min(dim=1))
+        return loss.mean()
+
+
